9bae21ed7d5d569f087bc20589d45c8ffb660148
Juan Barreneche <snipperme@gmail.com>
2014-07-16 18:14:16 -0300

    Be less strict with guessed scope when filtering options

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ maintainer       "Restorando S.A."
 maintainer_email "ops@restorando.com"
 license          "MIT"
 description      "Library cookbook for service discovery"
-version          "0.1.0"
+version          "0.1.1"
 
 depends "ip_finder", "0.1.0"
